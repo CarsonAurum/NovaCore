bc11444a47b52080c517ac4b91b04f3c6d5171cd
Carson R <53197646+CarsonAurum@users.noreply.github.com>
2022-10-17 10:47:24 -0500

    modules finished (finally lol)

---
+++ Sources/Extensions/SwiftUI/Color++.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  
+//
+//  Created by Carson Rau on 10/16/22.
+//
+
+#if canImport(SwiftUI)
+import SwiftUI
+
+#if canImport(UIKit)
+import UIKit
+#elseif canImport(AppKit)
+import AppKit
+#endif
+
+public extension Color {
+    @available(iOS 14.0, macOS 13, *)
+    var components: (red: CGFloat, green: CGFloat, blue: CGFloat, opacity: CGFloat) {
+
+        #if canImport(UIKit)
+        typealias NativeColor = UIColor
+        #elseif canImport(AppKit)
+        typealias NativeColor = NSColor
+        #endif
+
+        var r: CGFloat = 0
+        var g: CGFloat = 0
+        var b: CGFloat = 0
+        var o: CGFloat = 0
+
+        guard NativeColor(self).getRed(&r, green: &g, blue: &b, alpha: &o) else {
+            // You can handle the failure here as you want
+            return (0, 0, 0, 0)
+        }
+
+        return (r, g, b, o)
+    }
+}
+#endif
